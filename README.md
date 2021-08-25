e769a7c14d37c4a969e08d8eb3675f2604c6e8e4
aateplyakovva <anna.teplyakovap@gmail.com>
2021-08-25 15:40:21 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Responsive CV-site using HTML CSS and JavaScript. 
+## Add light/dark theme & export pdf.
+## Site:
+>
---
+++ assets/js/dist/main.dev.js
@@ -0,0 +1,139 @@
+"use strict";
+
+/*==================== SHOW MENU ====================*/
+var showMenu = function showMenu(toggleId, navId) {
+  var toggle = document.getElementById(toggleId);
+  var nav = document.getElementById(navId); //Validate that variables exist
+
+  if (toggle && nav) {
+    toggle.addEventListener('click', function () {
+      //We add the show-menu class to the div tag with the nav__menu class
+      nav.classList.toggle('show-menu');
+    });
+  }
+};
+
+showMenu('nav-toggle', 'nav-menu');
+/*==================== REMOVE MENU MOBILE ====================*/
+
+var navLink = document.querySelectorAll('.nav__link');
+
+function linkAction() {
+  var navMenu = document.getElementById('nav-menu'); //When we click on each nav__link, we remote the show-menu class
+
+  navMenu.classList.remove('show-menu');
+}
+
+;
+navLink.forEach(function (n) {
+  return n.addEventListener('click', linkAction);
+});
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+
+var sections = document.querySelectorAll('section[id]');
+
+function scrollActive() {
+  var scrollY = window.pageYOffset;
+  sections.forEach(function (current) {
+    var sectionHeight = current.offsetHeight;
+    var sectionTop = current.offsetTop - 50;
+    var sectionId = current.getAttribute('id');
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link');
+    } else {
+      document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link');
+    }
+  });
+}
+
+window.addEventListener('scroll', scrollActive);
+/*==================== SHOW SCROLL TOP ====================*/
+
+function scrollTop() {
+  var scrollTop = document.getElementById('scroll-top'); //when the scroll is higher than 200 vh, add the show-scroll class to the a tag with the scroll-top
+
+  if (this.scrollY >= 200) {
+    scrollTop.classList.add('show-scroll');
+  } else {
+    scrollTop.classList.remove('show-scroll');
+  }
+}
+
+window.addEventListener('scroll', scrollTop);
+/*==================== DARK LIGHT THEME ====================*/
+
+var themeButton = document.getElementById('theme-button');
+var darkTheme = 'dark-theme';
+var iconTheme = 'bx-sun'; //Previously selected topic (if user selected)
+
+var selectedTheme = localStorage.getItem('selected-theme');
+var selectedIcon = localStorage.getItem('selected-icon'); //We obtain current theme that the interface has by validating the dark-theme class
+
+var getCurrentTheme = function getCurrentTheme() {
+  return document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+};
+
+var getCurrentIcon = function getCurrentIcon() {
+  return themeButton.classList.contains(iconTheme) ? 'bx-moon' : 'bx-sun';
+}; //We validate if the user previously chose a topic
+
+
+if (selectedTheme) {
+  //If the validation is fulfilled, we ask what the issue was to khow we activated or deacrivated the dark theme
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
+  themeButton.classList[selectedTheme === 'uil-moon' ? 'add' : 'remove'](iconTheme);
+} //active / deactive the theme manually with the button
+
+
+themeButton.addEventListener('click', function () {
+  //add or remove the dark / icon theme
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme); //we save the theme and the current icon that the user chose
+
+  localStorage.setItem('selected-theme', getCurrentTheme());
+  localStorage.setItem('selected-icon', getCurrentIcon());
+});
+/*==================== REDUCE THE SIZE AND PRINT ON AN A4 SHEET ====================*/
+// function scaleCv() {
+//     document.body.classList.add('scale-cv')
+// }
+
+/*==================== REMOVE THE SIZE WHEN THE CV IS DOWNLOADED ====================*/
+// function removeScale() {
+//     document.body.classList.remove('scale-cv')
+// }
+
+/*==================== GENERATE PDF ====================*/
+// PDF generated area
+// let areaCv = document.getElementById('area-cv');
+// let resumeButton = document.getElementById('resume-button')
+// // Html2pdf options
+// let opt = {
+//     margin: 2,
+//     filename: 'Anna_Teplyakova.pdf',
+//     image: {
+//         type: 'jpeg',
+//         quality: 0.98
+//     },
+//     html2canvas: {
+//         scale: 4
+//     },
+//     jsPDF: {
+//         format: 'a4',
+//         orientation: 'portrait'
+//     }
+// }
+// Function to call areaCv and Html2Pdf options 
+// function generateResume() {
+//     html2pdf(areaCv);
+// }
+// When the button is clicked, it executes the three functions
+// resumeButton.addEventListener('click', () => {
+//     // 1. The class .scale-cv is added to the body, where it reduces the size of the elements
+//     scaleCv()
+// })
+// 2. The PDF is generated
+// generateResume()
+// 3. The .scale-cv class is removed from the body after 5 seconds to return to normal size.
+// setTimeout(removeScale, 5000)
